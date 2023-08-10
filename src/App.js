45ba7ff89fb679b09427caff52c4bd5da31f98f8
ysaleem <yusuf_saleem@sfu.ca>
2023-08-09 23:40:23 -0700

    route potential fix 2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Success from './successPage'
 
 function App() {
     return (
-        <Router>
+        <Router basename="/lang">
             <Routes>
                 <Route path="/lang" element={<Login />} />
                 <Route path="/lang/success" element={<Success/>} />
