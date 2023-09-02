936afb010f77909eb88b76fa3be5fbe17ecc3fd1
ysaleem <ysaleem@sfu.ca>
2023-09-01 17:22:08 -0700

    major ui changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from "./loginPage"
 import Success from './successPage'
+import SubmitField from './submitField'
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/dashboard" element={<Success/>} />
+                <Route path="/test" element={<SubmitField/>} />
                 <Route path="/*" element={<Navigate to="" replace />} />
             </Routes>
         </Router>
